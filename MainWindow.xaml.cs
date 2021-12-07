056daa4e056b1300a7712da035f01c47ed64b17f
Mateusz Swietlicki <mateusz@swietlicki.net>
2021-12-07 12:02:06 +0100

    Init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ms_video_cut
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+
+            // Begin dragging the window
+            this.DragMove();
+        }
+
+        private void media_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            sliderSeek.Minimum = 0;
+            sliderSeek.Maximum = media.NaturalDuration.TimeSpan.TotalSeconds;
+        }
+    }
+}
