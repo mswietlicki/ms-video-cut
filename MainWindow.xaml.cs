35fc85537dbd5b38bd849f8759f9df3a0ab3befe
Mateusz Swietlicki <mateusz@swietlicki.net>
2021-12-08 15:50:09 +0100

    Gu.Wpf.Media

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Gu.Wpf.Media;
+using Microsoft.Win32;
 
 namespace ms_video_cut
 {
@@ -33,10 +35,51 @@ namespace ms_video_cut
             this.DragMove();
         }
 
-        private void media_MediaOpened(object sender, RoutedEventArgs e)
+        private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
         {
-            sliderSeek.Minimum = 0;
-            sliderSeek.Maximum = media.NaturalDuration.TimeSpan.TotalSeconds;
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = $"Media files|{this.MediaElement.VideoFormats}|All files (*.*)|*.*"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                this.MediaElement.SetCurrentValue(MediaElementWrapper.SourceProperty, new Uri(openFileDialog.FileName));
+            }
+        }
+
+        private void OnToggleFullScreenExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            // if (this.WindowStyle == WindowStyle.SingleBorderWindow)
+            // {
+            //     this.MediaElement.Stretch = Stretch.UniformToFill;
+            //     this.WindowStyle = WindowStyle.None;
+            //     this.SizeToContent = SizeToContent.Manual;
+            //     this.WindowState = WindowState.Maximized;
+            // }
+            // else
+            // {
+            //     this.MediaElement.Stretch = Stretch.UniformToFill;
+            //     this.WindowStyle = WindowStyle.SingleBorderWindow;
+            //     this.SizeToContent = SizeToContent.WidthAndHeight;
+            //     this.WindowState = WindowState.Normal;
+            // }
+
+            e.Handled = true;
+        }
+
+        private void OnEndFullScreenCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = this.WindowState == WindowState.Maximized && this.WindowStyle == WindowStyle.None;
+        }
+
+        private void OnEndFullScreenExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            // this.MediaElement.Stretch = Stretch.UniformToFill;
+            // this.WindowStyle = WindowStyle.SingleBorderWindow;
+            // this.SizeToContent = SizeToContent.WidthAndHeight;
+            // this.WindowState = WindowState.Normal;
+            e.Handled = true;
         }
     }
 }
