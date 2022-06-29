ffefbba04245a816d54f83049464bfb6f1de6f34
Mateusz Swietlicki <mateusz@swietlicki.net>
2022-06-29 22:54:57 +0200

    Update progress and Pause

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,7 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Unosquare.FFME;
+using System.Windows.Threading;
 
 namespace ms_video_cut
 {
@@ -23,11 +24,12 @@ namespace ms_video_cut
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
 
-            Unosquare.FFME.Library.FFmpegDirectory = @"C:\tools\ffmpeg\bin";
+            // Unosquare.FFME.Library.FFmpegDirectory = @"C:\ProgramData\chocolatey\bin\";
 
             // var vlcLibDirectory = new DirectoryInfo(@"C:\Program Files\VideoLAN\VLC");
 
@@ -39,33 +41,68 @@ namespace ms_video_cut
             // // media.SourceProvider.CreatePlayer(vlcLibDirectory, options);
             // // media.SourceProvider.MediaPlayer.Play(new Uri(@"C:\Users\mateu\Videos\Dron\DJI_0147.MP4"));
 
+            media.MediaOpened += media_MediaOpened;
             media.MouseLeftButtonDown += media_MouseLeftButtonDown;
-            this.Loaded += window_OnPageLoad;
+            media.Play();
+            this.KeyUp += media_KeyUp;
 
-        }
-        private void window_OnPageLoad(object sender, RoutedEventArgs e)
-        {
-            //media.Open(new Uri(@"C:\Users\mateu\Videos\Dron\DJI_0193.MP4"));
+            timer.Interval = TimeSpan.FromMilliseconds(10);
+            timer.Tick += timer_Tick;
+            timer.Start();
+
         }
 
         protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
         {
             base.OnMouseLeftButtonDown(e);
-
-            // Begin dragging the window
             this.DragMove();
         }
 
+        private void media_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Space:
+                    PauseToggle();
+                    break;
+            }
+        }
+
+        private void PauseToggle()
+        {
+            if (GetMediaState(media) == MediaState.Play)
+                media.Pause();
+            else
+                media.Play();
+        }
+
+        private MediaState GetMediaState(MediaElement media)
+        {
+            var helper = typeof(MediaElement)
+                .GetField("_helper", BindingFlags.NonPublic | BindingFlags.Instance)
+                ?.GetValue(media);
+
+            return helper
+                ?.GetType()
+                ?.GetField("_currentState", BindingFlags.NonPublic | BindingFlags.Instance)
+                ?.GetValue(helper) as MediaState? ?? MediaState.Close;
+        }
+        
         private void media_MediaOpened(object sender, RoutedEventArgs e)
         {
             sliderSeek.Minimum = 0;
-            // sliderSeek.Maximum = media.
+            sliderSeek.Maximum = media.NaturalDuration.TimeSpan.TotalMilliseconds / 100;
         }
 
         private void media_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ClickCount == 2)
                 this.WindowState = WindowState.Maximized;
+        }
+
+        private void timer_Tick(object? sender, EventArgs e)
+        {
+            sliderSeek.Value = media.Position.TotalMilliseconds / 100;
         }
     }
 }
